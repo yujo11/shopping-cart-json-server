a688ebb4d53d07a4dffc52b17e11e72ee86cdf8b
yujo <yujo.dev@gmail.com>
2022-01-31 23:25:29 +0900

    test: server test

--- client/index.js
+++ client/index.js
@@ -1,11 +1,23 @@
-const $app = document.querySelector("#app");
-
 axios.defaults.baseURL = "http://localhost:3003";
+
+/*
+   GET  find All
+*/
 
 const products = await axios.get("/products");
 const carts = await axios.get("/carts");
 const orders = await axios.get("/orders");
 
-console.log(products);
-console.log(carts);
-console.log(orders);
+console.log("findAll :", products);
+console.log("findAll :", carts);
+console.log("findAll :", orders);
+
+/*
+   GET  find by ID
+*/
+
+const product = await axios.get("/products/1");
+const order = await axios.get("/orders/1");
+
+console.log("findById :", product);
+console.log("findById :", order);
